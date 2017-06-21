768a3fbc690e539cabe273fafd6351ee65cceb0d
BenjaminM Abdel-Karim <benjaminmabdel-karim@BenjaminMs-MacBook.local>
2017-06-21 20:04:01 +0200

    ALALAL

--- Modelguete und Sensitivitaet (LaTeX)/bSkriptModellperformance.m
+++ Modelguete und Sensitivitaet (LaTeX)/bSkriptModellperformance.m
@@ -12,7 +12,26 @@ addpath('Funktionsbibliothek/MATLAB2Tikiz/src');
 
 %% Import the Data
 % Load the Data in a new matrix
-mData = xlsread('ModelleGuete.xlsx');
+%mData = xlsread('ModelleGuete.xlsx');
+
+% Die NaiveBayes
+% Jeder Modellwert enthaelt eine ID von 1 bis n. Messung erfolgt ueber die
+% Laenge der Eintrage. Wird fuer den Boxplot benotigt.
+vNaiveBayesValues=  csvread('Datenbank/NaiveBayes.csv');
+vNaiveBayesID = zeros(length(vNaiveBayesValues),1);
+vNaiveBayesID = vNaiveBayesID+1; % Index 1. 
+
+% SVM
+vSVMValues =  csvread('Datenbank/SVM.csv');
+vSVMValuesID = zeros(length(vSVMValues),1);
+vSVMValuesID = vSVMValuesID+2; % Index 2.
+
+
+%% Setze das Ergebnisfeld zusammen
+% Das Ergebnisfeld fuer die Modelle wird zusammen gesetzt. 
+mData = [vNaiveBayesValues, vNaiveBayesID;
+        vSVMValues, vSVMValuesID
+];
 
 
 %% Creat the Boxplot for each Modell
@@ -22,11 +41,11 @@ mData = xlsread('ModelleGuete.xlsx');
 % (sensitivtitat).
 
 figure;
-boxplot(mData(:,2), mData(:,1))
+boxplot(mData(:,1), mData(:,2))
 title('Trefferquote und Modellrobusheit');
 grid on;
 ylabel('Trefferqueto');
 xlabel('Modelle');
-xticklabels({'LRM','SVM','ANN','Tree','LPT',});
-
+xticklabels({'Naive Bayes', 'SVM'});
+% xticklabels({'LRM','SVM','ANN','Tree','LPT',});
 matlab2tikz('Abbildungen/ModelleGueteUndRobustheit.tex');
